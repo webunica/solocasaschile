a5b59220a60d922c63afdf3947fca3c7c082d6b6
WebUnica Chile <javier.millar@gmail.com>
2026-02-26 23:55:12 -0300

    fix: remove invalid fallback projectId from sanity.client.ts

--- src/lib/sanity.client.ts
+++ src/lib/sanity.client.ts
@@ -1,6 +1,6 @@
 import { createClient } from 'next-sanity'
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'tu_project_id' // Reemplazar con env file en prod
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
 export const apiVersion = '2024-02-26'
 
@@ -8,5 +8,5 @@ export const sanityClient = createClient({
     projectId,
     dataset,
     apiVersion,
-    useCdn: true, // false si necesitas revalidación rápida
+    useCdn: true,
 })
