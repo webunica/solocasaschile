df4b1f855f6d08f650382fe1556feefa710abda1
WebUnica Chile <javier.millar@gmail.com>
2026-03-05 23:26:40 -0300

    feat(seo): agregar Google Analytics (G-9TPXMBDBGK)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Nunito } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import { Footer } from "@/components/Footer";
@@ -108,6 +109,21 @@ export default function RootLayout({
   return (
     <html lang="es" className="scroll-smooth">
       <head>
+        {/* Google Analytics (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-9TPXMBDBGK"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-9TPXMBDBGK');
+          `}
+        </Script>
+
         {/* Preconnect to external origins for better LCP/FCP */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
